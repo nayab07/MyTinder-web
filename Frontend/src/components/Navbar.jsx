c3e7e33d8f7026a63f93ca0361f724cc942f95e6
nayab shams <nayabshams86@gmail.com>
2025-02-16 21:09:41 +0530

    change in navbar

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -51,14 +51,14 @@ const Navbar = () => {
             <Link to="/login" className="btn btn-ghost">Login</Link>
           </div>
         )}
-        {/* Mobile Hamburger Menu for small screens */}
-        {/* <div className="block md:hidden">
+         {/* Mobile Hamburger Menu for small screens  */}
+         <div className="block md:hidden">
           <button className="btn btn-ghost" onClick={() => alert('Mobile menu clicked')}>
             <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
             </svg>
           </button>
-        </div> */}
+        </div>
       </div>
     </div>
   )
